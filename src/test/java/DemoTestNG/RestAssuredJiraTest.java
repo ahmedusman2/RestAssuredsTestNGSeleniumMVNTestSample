c20d0bef71612f4ffa9c1e9a2c2341dd813b3dae
Ahmed Usman <ahmed.usman_shani@hotmail.com>
2023-06-01 19:05:11 +0400

    feat: added code for adding an attachment to jira issue via rest assured.

--- src/test/java/DemoTestNG/RestAssuredJiraTest.java
+++ src/test/java/DemoTestNG/RestAssuredJiraTest.java
@@ -3,6 +3,8 @@ package DemoTestNG;
 import io.restassured.RestAssured;
 import io.restassured.filter.session.SessionFilter;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
 
 public class RestAssuredJiraTest {
@@ -15,6 +17,7 @@ public class RestAssuredJiraTest {
         //         3. BaseURI
         //         4. Body of the request (if any)
         //         5. Session filter (if any)
+        //         6. Multi Path for attachments(if any)
         // when():
         //         1. http method,
         //         2. resource
@@ -26,9 +29,9 @@ public class RestAssuredJiraTest {
         RestAssured.baseURI = "http://localhost:8080";
         SessionFilter session = new SessionFilter();
 
-        // POST: LOGIN Request to Jira
+
         String loginResponse = given()
-                .header("content-type", "application/json")
+                .header("Content-Type", "application/json")
                 .body("{ \"username\": \"ahmed.usman_shani\", \"password\": \"Redhat111!\" }").log().all()
                 .filter(session)
                 .when()
@@ -49,5 +52,16 @@ public class RestAssuredJiraTest {
                         "}").filter(session)
                 .when().post("/rest/api/2/issue/{id}/comment").then().assertThat().statusCode(201);
 
+        // POST: Add attachment to a Jira issue
+        given().header("X-Atlassian-Token", "no-check").filter(session).pathParams("id", "10007")
+                .header("Content-Type", "multipart/form-data")
+                .multiPart("file", new File("jiraDummyattachmentTextFile.txt"))
+                .when().post("/rest/api/2/issue/{id}/attachments")
+                .then().log().all().assertThat().statusCode(200);
+
+
     }
+
+
+
 }
