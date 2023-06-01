efa8b6872a9853b9b62ebe15d720e57ee5f1847e
Ahmed Usman <ahmed.usman_shani@hotmail.com>
2023-06-01 16:33:14 +0400

    feat: added sessionfilter and e2e create login call and add comment call

--- src/test/java/DemoTestNG/RestAssuredJiraTest.java
+++ src/test/java/DemoTestNG/RestAssuredJiraTest.java
@@ -1,6 +1,7 @@
 package DemoTestNG;
 
 import io.restassured.RestAssured;
+import io.restassured.filter.session.SessionFilter;
 
 import static io.restassured.RestAssured.given;
 
@@ -12,7 +13,8 @@ public class RestAssuredJiraTest {
         // given():1. Header of the request,
         //         2. Query parameters/Path parameters,
         //         3. BaseURI
-        //         4. Body of the request
+        //         4. Body of the request (if any)
+        //         5. Session filter (if any)
         // when():
         //         1. http method,
         //         2. resource
@@ -22,24 +24,30 @@ public class RestAssuredJiraTest {
         //         4. Response body
 
         RestAssured.baseURI = "http://localhost:8080";
+        SessionFilter session = new SessionFilter();
 
         // POST: LOGIN Request to Jira
-        String loginResponse = given().header("content-type", "application/json").body("{ \"username\": \"ahmed.usman_shani\", \"password\": \"Redhat111!\" }")
-                .when().post("/rest/auth/1/session")
-                .then().assertThat().statusCode(200).extract().response().asString();
+        String loginResponse = given()
+                .header("content-type", "application/json")
+                .body("{ \"username\": \"ahmed.usman_shani\", \"password\": \"Redhat111!\" }").log().all()
+                .filter(session)
+                .when()
+                .post("/rest/auth/1/session")
+                .then()
+                .assertThat().statusCode(200).extract().response().asString();
+
         System.out.println(loginResponse);
-/*
+
 
         //POST: ADD comment to issue to a JIRA issue:
         given().pathParams("id", "10007").log().all().header("Content-Type", "application/json").body("{\n" + // what ever you define here will be treated as path variable
-                "    \"body\": \"RestAssured: comment added from RestAssured\",\n" +
-                "    \"visibility\": {\n" +
-                "        \"type\": \"role\",\n" +
-                "        \"value\": \"Administrators\"\n" +
-                "    }\n" +
-                "}").when().post("/rest/api/2/issue/{id}/comment").
-        // resource contains a path parameter so it shouldbe written with  {}
-*/
+                        "    \"body\": \"RestAssured: comment added from RestAssured2\",\n" +
+                        "    \"visibility\": {\n" +
+                        "        \"type\": \"role\",\n" +
+                        "        \"value\": \"Administrators\"\n" +
+                        "    }\n" +
+                        "}").filter(session)
+                .when().post("/rest/api/2/issue/{id}/comment").then().assertThat().statusCode(201);
 
     }
 }
